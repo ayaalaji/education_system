faef364dbb0eb9b323f9e9894816f1fac2519ee8
XPRISTO <1199ali12dr@gmail.com>
2024-11-28 14:48:58 +0300

    make CRUD for user /move userController to Api\

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\RoleController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
