5db15d8fc6cf42ff8c669df60ac15233a2d262eb
samerabbas77 <samer23abbas@gmail.com>
2024-12-25 11:11:49 +0300

    Fetat(course) : add course controller with model, migration and service. Implemented the CourseController with all associated classes, models, and migrations. The course module includes relationships with users, teachers, categories, and validation for course duration and dates.

--- database/migrations/2024_12_06_143008_create_course_user_table.php
+++ database/migrations/2024_12_06_143008_create_course_user_table.php
@@ -11,12 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('course_user', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->text('description')->nullable();;
-            $table->foreignId('teacher_id')->constrained();
-            $table->index('name');
+            $table->foreignId('course_id')->constrained()->onDelete('cascade'); 
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); 
+           
             $table->timestamps();
         });
     }
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('course_user');
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,17 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
         $this->call([
             PermissionSeeder::class,
-        ]);
-        $this->call([
             TeacherSeeder::class,
+            CategorySeeder::class,
+            CourseSeeder::class
         ]);
+   
     }
 }
